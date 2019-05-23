68ac8a8079375dc76ca5f5173dfbe34ba68de6d3
Daniel Lucas <dlsbdaniel@gmail.com>
2019-05-23 10:28:49 -0300

    Using the label expression with svelte

--- first-project/src/App.svelte
+++ first-project/src/App.svelte
@@ -1,8 +1,22 @@
 <script>
   let name = 'Daniel';
   let age = 25;
+
+  $: uppercaseName = name.toUpperCase();
+
+  $: console.log(name);
+
+  $: if(name === 'Lucas') {
+    console.log('It runs!');
+    age = 25;
+  }
+
   function incrementAge() {
     age += 1;
+  }
+
+  function changeName() {
+    name = 'Lucas';
   }
 </script>
 
@@ -12,5 +26,6 @@
 	}
 </style>
 
-<h1>Hello {name}, my age is {age}!</h1>
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
 <button on:click="{incrementAge}">Change Age</button>
+<button on:click="{changeName}">Change Name</button>
