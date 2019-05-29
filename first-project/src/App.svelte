ae96d79a3ff2594a92abaa6634cbe227694ada5e
Daniel Lucas <dlsbdaniel@gmail.com>
2019-05-29 09:06:02 -0300

    Outputting content conditionally

--- first-project/src/App.svelte
+++ first-project/src/App.svelte
@@ -5,6 +5,20 @@
   let title = "";
   let image = "";
   let description = "";
+  let formState = 'empty';
+
+  function addContact() {
+    if(
+      name.trim().length == 0 || 
+      title.trim().length == 0 || 
+      image.trim().length == 0 ||
+      description.trim().length == 0
+    ){
+      formState = 'invalid';
+      return;
+    }
+    formState = 'done';
+  }
 </script>
 
 <style>
@@ -33,4 +47,15 @@
   </div>
 </div>
 
-<ContactCard userName={name} jobTitle={title} {description} userImage={image} />
+<button on:click={addContact}>Add contact card</button>
+{#if formState === 'done'}
+  <ContactCard 
+    userName={name} 
+    jobTitle={title} 
+    {description} 
+    userImage={image} />
+{:else if formState === 'invalid'}
+  <p>Invalid Input.</p>
+{:else}
+  <p>Please enter some data and hit the button!</p>
+{/if}
