34dc5ec767d630daf8961d3ef21f8c6bc0b3dd4c
Daniel Lucas <dlsbdaniel@gmail.com>
2019-05-31 16:18:12 -0300

    Preparing to create a new component

--- course-project/src/App.svelte
+++ course-project/src/App.svelte
@@ -1,5 +1,26 @@
 <script>
   import Header from './UI/Header.svelte';
+
+  const meetups = [
+    {
+      id: 'm1',
+      title: 'Coding Bootcamp',
+      subtitle: 'Learn to code in 2 hours',
+      description: 'In this meetup, we will have some experts teach you how to code in 2 hours.',
+      imageUrl: 'http://asdwgertebrtbrbrbrbrb12e1',
+      address: '27th Nerd Road, 32523 New York',
+      contactEmail: 'code@test.com'
+    },
+    {
+      id: 'm2',
+      title: 'Swim Together',
+      subtitle: 'Let\'s go for some swimming',
+      description: 'We will simply swim some rounds!',
+      imageUrl: 'http://asdwgertebr12312vtbtghnttt1',
+      address: '27th Nerd Road, 32523 New York',
+      contactEmail: 'swim@test.com'
+    },
+  ];
 </script>
 
 <style>
